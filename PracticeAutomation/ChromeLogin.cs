5de9c3aa5122b8c8057ae32a1b66d093f227441d
Manisha Bhanot DA <manisha.bhanot@directaxis.co.za>
2020-02-04 11:58:04 +0530

    Created two clases and did same test case in both the class

---
+++ PracticeAutomation/ChromeLogin.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace PracticeAutomation
+{
+    public class FB
+    {
+        IWebDriver Driver = new ChromeDriver();
+        [SetUp]
+        public void start()
+        {
+            Driver.Navigate().GoToUrl("http://www.facebook.com");
+            Driver.Manage().Window.Maximize();
+        }
+        [Test]
+        public void Operations()
+        {
+            Driver.FindElement(By.Name("email")).SendKeys("manishabhanot96@gmail.com");
+            Driver.FindElement(By.Name("pass")).SendKeys("kudrat@123");
+            Driver.FindElement(By.Id("loginbutton")).Click();
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+            js.ExecuteScript("window.scrollTo(0, document.body.scrollHeight)");
+        }
+        //  [TearDown]
+        public void Stop()
+        {
+            Driver.Quit();
+        }
+    }
+}
---
+++ PracticeAutomation/firefoxLogin.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace PracticeAutomation
+{
+    class firefoxLogin
+    {
+        IWebDriver Driver = new FirefoxDriver();
+        [SetUp]
+        public void start()
+        {
+            Driver.Navigate().GoToUrl("http://www.facebook.com");
+            Driver.Manage().Window.Maximize();
+        }
+        [Test]
+        public void Operations()
+        {
+            Driver.FindElement(By.Name("email")).SendKeys("manishabhanot96@gmail.com");
+            Driver.FindElement(By.Name("pass")).SendKeys("kudrat123");
+            Driver.FindElement(By.Id("loginbutton")).Click();
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+            js.ExecuteScript("window.scrollTo(0, document.body.scrollHeight)");
+
+
+
+        }
+    }
+}
